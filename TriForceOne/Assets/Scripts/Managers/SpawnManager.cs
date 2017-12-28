b433760e0b5c0ef1e34fe79fa151222020b3f168
AlexanderMalthed <34477934+AlexanderMalthed@users.noreply.github.com>
2017-12-28 16:38:31 +0100

    Level 5 & 6 further testing
    En ändrig i spawnmanager låter spelaren vinna efter att ha klarat nivå 6.
    Ny vinsttext lades även in för de två nivåerna.
    
    Ändringen är "-1" i följande:
     if (thisWave >= waves.Length-1)

--- TriForceOne/Assets/Scripts/Managers/SpawnManager.cs
+++ TriForceOne/Assets/Scripts/Managers/SpawnManager.cs
@@ -143,7 +143,7 @@ public class SpawnManager : MonoBehaviour
     {
         enemyCount--;
         if (enemyCount <= 0)
-        {
+        {          
             GameObject[] towers = GameObject.FindGameObjectsWithTag("Tower");
             foreach (GameObject tower in towers)
             {
@@ -152,7 +152,8 @@ public class SpawnManager : MonoBehaviour
                     tower.GetComponent<CombatTower>().AmmoRestock();
                 }
             }
-            if (thisWave >= waves.Length)
+
+            if (thisWave >= waves.Length-1)
             {
                 victoryScreen.SetActive(true);
                 Time.timeScale = 0;
